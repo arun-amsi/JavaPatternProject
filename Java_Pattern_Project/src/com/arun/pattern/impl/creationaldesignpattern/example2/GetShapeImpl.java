da344110bc7c240f4bf019d0cb952cea5bba8027
arun-amsi <grazyarun@gmail.com>
2016-10-01 22:05:50 +0530

    Example 2
    Signed-off-by: arun-amsi <grazyarun@gmail.com>

--- Java_Pattern_Project/src/com/arun/pattern/impl/creationaldesignpattern/example2/GetShapeImpl.java
+++ Java_Pattern_Project/src/com/arun/pattern/impl/creationaldesignpattern/example2/GetShapeImpl.java
@@ -5,7 +5,9 @@ public class GetShapeImpl {
 	public static void main(String a[]) {
 		GetShapeFactory factory = new GetShapeFactory();
 		factory.getShape("Circle").draw();
+		
 		factory.getShape("Square").draw();
+		
 		factory.getShape("Rectangle").draw();
 	}
 
