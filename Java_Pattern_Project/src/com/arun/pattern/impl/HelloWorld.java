9b8bb1569eeebb3e5f6813f1c13fce3e5a50bffa
arun-amsi <grazyarun@gmail.com>
2016-10-01 20:23:51 +0530

    Pattern java project setup

---
+++ Java_Pattern_Project/src/com/arun/pattern/impl/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.arun.pattern.impl;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World...");
+	}
+}
