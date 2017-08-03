0e7035530de4d1571101fbece414581c48393fe3
arun-amsi <grazyarun@gmail.com>
2017-08-03 10:05:58 +0530

    Null Check

--- NullCheckUtil.java
+++ NullCheckUtil.java
@@ -1,1 +1,7 @@
-gfgs
+public static boolean notEmpty(String value){
+	return (value ! = null && value.trim().length > 0 );
+}
+
+public static boolean isEmpty(String value){
+	return (!notEmpty(value));
+}
